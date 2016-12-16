9e275bbd8a1ba10f23bc6da2399fa0a65cad1f13
hailindai <hailin_dai@163.com>
2016-12-16 10:23:54 +0800

    app use butter knife

--- app/src/main/java/com/dreamguard/androidusbcamera/USBCameraActivity.java
+++ app/src/main/java/com/dreamguard/androidusbcamera/USBCameraActivity.java
@@ -9,8 +9,13 @@ import android.widget.Toast;
 import com.dreamguard.api.CameraType;
 import com.dreamguard.api.KDXCamera;
 import com.dreamguard.api.CameraViewInterface;
+import com.dreamguard.widget.UVCCameraTextureView;
 
 import java.util.HashMap;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  *  create by hailin.dai 2016.12.10.
@@ -22,99 +27,59 @@ public class USBCameraActivity extends AppCompatActivity {
 
     private static final int PREVIEW_HEIGHT = 720;
 
-    private CameraViewInterface mUVCCameraView;
-
     private KDXCamera camera;
 
-    private Button record;
+    @Bind(R.id.camera_view)
+    protected UVCCameraTextureView mCameraView;
 
-    private Button captureStill;
 
-    private Button normal;
+    @OnClick(R.id.open) void open(){
+        if(!camera.isCameraOpened()){
+            boolean ret = camera.open(0);
+            if(!ret){
+                Toast.makeText(USBCameraActivity.this, "NO_USB_DEVICE", Toast.LENGTH_SHORT).show();
+            }else {
+                camera.setPreviewSize(PREVIEW_WIDTH,PREVIEW_HEIGHT);
+                camera.setPreviewTexture(mCameraView.getSurfaceTexture());
+                camera.startPreview();
+            }
+        }
+    }
+    @OnClick(R.id.close) void close(){
+        if(camera.isCameraOpened()){
+           camera.close();
+        }
+    }
+    @OnClick(R.id.captureStill) void captureStill(){
+        if(camera.isCameraOpened()){
+            Toast.makeText(USBCameraActivity.this, "Captured", Toast.LENGTH_SHORT).show();
+            camera.captureStill();
+        }
+    }
+    @OnClick(R.id.record) void record(){
+        if(camera.isCameraOpened() && !camera.isRecording()){
+            Toast.makeText(USBCameraActivity.this, "startRecording", Toast.LENGTH_SHORT).show();
+            camera.startRecording();
+        }
+        if(camera.isCameraOpened() && camera.isRecording()){
+            Toast.makeText(USBCameraActivity.this, "stopRecording", Toast.LENGTH_SHORT).show();
 
-    private Button stereo;
+            camera.stopRecording();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usbcamera);
-        final View view = findViewById(R.id.camera_view);
-        record = (Button) findViewById(R.id.record);
-        captureStill = (Button) findViewById(R.id.captureStill);
-        normal = (Button) findViewById(R.id.showNormal);
-        stereo = (Button) findViewById(R.id.showStereo);
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(!camera.isCameraOpened()){
-                    boolean ret = camera.open(0);
-                    if(!ret){
-                        Toast.makeText(USBCameraActivity.this, "NO_USB_DEVICE", Toast.LENGTH_SHORT).show();
-                    }else {
-                        camera.setPreviewSize(PREVIEW_WIDTH,PREVIEW_HEIGHT);
-                        camera.setPreviewTexture(mUVCCameraView.getSurfaceTexture());
-                        camera.startPreview();
-                    }
-                }else {
-                    camera.close();
-                }
-            }
-        });
+        ButterKnife.bind(this);
 
-        captureStill.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(camera.isCameraOpened()){
-                    Toast.makeText(USBCameraActivity.this, "Captured", Toast.LENGTH_SHORT).show();
-                    camera.captureStill();
-                }
-            }
-        });
-        record.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(camera.isCameraOpened() && !camera.isRecording()){
-                    camera.startRecording();
-                }
-                if(camera.isCameraOpened() && camera.isRecording()){
-                    camera.stopRecording();
-                }
-            }
-        });
+        mCameraView.setAspectRatio(PREVIEW_WIDTH / (float)PREVIEW_HEIGHT);
 
-
-        mUVCCameraView = (CameraViewInterface)view;
-        mUVCCameraView.setAspectRatio(PREVIEW_WIDTH/2 / (float)PREVIEW_HEIGHT);
         camera = new KDXCamera();
         camera.init(this);
         camera.setCameraType(CameraType.C3D_SBS);
 
-        normal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                HashMap<String ,String> rendererMode = new HashMap<String, String>();
-                rendererMode.put("rendererMode","0");
-                mUVCCameraView.setRendererParam(rendererMode);
-            }
-        });
-        stereo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                HashMap<String ,String> rendererMode = new HashMap<String, String>();
-                rendererMode.put("rendererMode","1");
-                mUVCCameraView.setRendererParam(rendererMode);
-            }
-        });
-    }
-
-    private void hideSystemUI() {
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        );
     }
 
     @Override
@@ -133,6 +98,16 @@ public class USBCameraActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         camera.destroy();
+    }
+
+    private void hideSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
     }
 
 
